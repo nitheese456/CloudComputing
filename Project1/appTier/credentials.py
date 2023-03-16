dd138682358d65d58af07a27a2799f1528d90370
nitheese456 <59762447+nitheese456@users.noreply.github.com>
2023-03-16 12:22:20 -0700

    App Tier Code

---
+++ Project1/appTier/credentials.py
@@ -0,0 +1,4 @@
+INPUT_BUCKET = 'input-image-files'
+OUTPUT_BUCKET = 'output-image-files'
+INPUT_QUEUE = 'https://sqs.us-east-1.amazonaws.com/378107157540/Request-Queue'
+OUTPUT_QUEUE = 'https://sqs.us-east-1.amazonaws.com/378107157540/Response-Queue'
